4e7833e620776b8fabcdbeaf510bfa14e866f99a
lash <dev@holbrook.no>
2018-05-01 16:00:06 +0200

    swarm/storage: Patchde LookupPrevious and BlockEstimator

--- swarm/storage/resource.go
+++ swarm/storage/resource.go
@@ -236,7 +236,6 @@ func (self *ResourceHandler) Validate(key Key, data []byte) bool {
 	} else if signature == nil {
 		return bytes.Equal(self.resourceHash(period, version, ens.EnsNode(name)), key)
 	}
-
 	digest := self.keyDataHash(key, parseddata)
 	addr, err := getAddressFromDataSig(digest, *signature)
 	if err != nil {
@@ -440,6 +439,29 @@ func (self *ResourceHandler) LookupLatest(ctx context.Context, nameHash common.H
 	return self.lookup(rsrc, nextperiod, 0, refresh, maxLookup)
 }
 
+// Returns the resource before the one currently loaded in the resource object
+//
+// Requires a synced resource object
+func (self *ResourceHandler) LookupPreviousByName(ctx context.Context, name string, maxLookup *ResourceLookupParams) (*resource, error) {
+	return self.LookupPrevious(ctx, ens.EnsNode(name), name, maxLookup)
+}
+
+func (self *ResourceHandler) LookupPrevious(ctx context.Context, nameHash common.Hash, name string, maxLookup *ResourceLookupParams) (*resource, error) {
+	rsrc := self.getResource(name)
+	if !rsrc.isSynced() {
+		return nil, NewResourceError(ErrNotSynced, "LookupPrevious requires synced resource.")
+	}
+	if rsrc.version > 1 {
+		rsrc.version--
+	} else if rsrc.lastPeriod == 1 {
+		return nil, NewResourceError(ErrNothingToReturn, "Current update is the oldest")
+	} else {
+		rsrc.version = 1
+		rsrc.lastPeriod--
+	}
+	return self.lookup(rsrc, rsrc.lastPeriod, rsrc.version, false, maxLookup)
+}
+
 // base code for public lookup methods
 func (self *ResourceHandler) lookup(rsrc *resource, period uint32, version uint32, refresh bool, maxLookup *ResourceLookupParams) (*resource, error) {
 
@@ -464,7 +486,6 @@ func (self *ResourceHandler) lookup(rsrc *resource, period uint32, version uint3
 	if maxLookup == nil {
 		maxLookup = self.queryMaxPeriods
 	}
-	log.Trace("resource lookuup", "period", period, "version", version, "limit", maxLookup.Limit, "max", maxLookup.Max)
 	for period > 0 {
 		if maxLookup.Limit && hops > maxLookup.Max {
 			return nil, NewResourceError(ErrPeriodDepth, fmt.Sprintf("Lookup exceeded max period hops (%d)", maxLookup.Max))
@@ -746,7 +767,6 @@ func (self *ResourceHandler) getBlock(ctx context.Context, name string) (uint64,
 	if err != nil {
 		return 0, err
 	}
-	log.Warn("blockheader", "header", blockheader)
 	return blockheader.Number.Uint64(), nil
 }
 
